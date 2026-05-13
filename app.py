8948c37a789b663928031df1f862c3ebe40c98ef
j-wojacka0520221 <juliawojacka13@gmail.com>
2026-05-13 12:48:23 +0100

    New Content Pages
    added the design, code, about and contact pages

--- app.py
+++ app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/design")
+def design():
+    return render_template("design.html")
+
+@app.route("/code")
+def code():
+    return render_template("code.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
